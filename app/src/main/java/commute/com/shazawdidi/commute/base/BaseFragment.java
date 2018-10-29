b072805a9227f5454da395b8520ecfb2cbb4154e
Shazalia Moawia <shazaliawd@gmail.com>
2018-10-29 14:26:10 +0200

    waleed project

---
+++ app/src/main/java/commute/com/shazawdidi/commute/base/BaseFragment.java
@@ -0,0 +1,28 @@
+package commute.com.shazawdidi.commute.base;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by shazalia on Sep, 2018
+ */
+public class BaseFragment extends Fragment {
+
+//  protected ViewDataBinding mBinding = null;
+
+  @Nullable @Override
+  public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+      @Nullable Bundle savedInstanceState) {
+    return super.onCreateView(inflater, container, savedInstanceState);
+  }
+
+  @Override public void onDestroyView() {
+    super.onDestroyView();
+//    mBinding = null;
+  }
+}
---
+++ app/src/main/java/commute/com/shazawdidi/commute/base/BasePresenter.java
@@ -0,0 +1,14 @@
+package commute.com.shazawdidi.commute.base;
+
+/**
+ * Created by shazalia on Sep, 2018
+ */
+public interface BasePresenter {
+  void onCreate();
+
+  void onStart();
+
+  void onStop();
+
+  void onDestroy();
+}
